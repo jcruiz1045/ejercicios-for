9b063befe2d6e2b66c99c488f6047d32185e3beb
jcruiz <artur.j.18@hotmail.com>
2023-06-20 22:10:56 -0500

    primer commit

---
+++ for.js
@@ -0,0 +1,130 @@
+/*ejercicio 1*/
+/*for ( var inpares = 1; inpares <= 50; inpares++) {
+    if (inpares % 2 !== 0) {
+        console.log(inpares);
+    }
+}*/
+
+/*ejercicio 2*/
+/*let pokemos = [
+  'bulbasaurio', 
+  'hiedrosaurio',
+  'venusauro',
+  'encantador',
+  'charmeleon',
+  'charizard',
+  'ardilla',
+  'tortuga de guerra',
+  'blastoise',
+  'oruga',
+  'metápodo',
+  'sin mantequilla',
+  'hierba',
+  'kakuna',
+  'beedrill',
+  'pidgey',
+  'pidgeotto',
+  'pidgeot',
+  'rattata',
+  'raticar',
+  'spearrow',
+  'fearow',
+  'ekans',
+  'arbok',
+  'pikachu',
+  'raichu',
+  'musaraña',
+  'barra de arena',
+  'nidoran-f',
+  'nidorina',
+  'nidoreina',
+  'nidoran-m',
+  'nidorino',
+  'nidoking',
+  'clefairy',
+  'clefible',
+  'vulpix',
+  'ninetales',
+  'jigglypuff',
+  'wigglytuff',
+  'zubat',
+  'golbat',
+  'raro',
+  'penumbra',
+  'pluma vil',
+  'paras',
+  'parásito',
+  'venona',
+  'polilla venenosa',
+  'diglett',
+  'dugtrio',
+  'maullido',
+  'persa',
+  'psyduck',
+  'dorado',
+  'mankey',
+  'principal',
+  'gruñir',
+  'arcano',
+  'poliwag',
+  'politorbellino',
+  'poliira',
+  'abre',
+  'kadabra',
+  'alakazam',
+  'machop',
+  'machoke',
+  'machamp',
+  'brote de campana',
+  'campana llorona',
+  'victreebel',
+  'tentacool',
+  'tentacruel',
+  'geodude',
+  'gravillero',
+  'golem',
+  'ponita',
+  'rápidash',
+  'lento',
+  'hermano lento',
+  'magnemita',
+  'magnetón',
+  'farfetchd',
+  'doduo',
+  'dodrio',
+  'seel',
+  'dewgong',
+  'más sombrío',
+  'muk',
+  'desgranadora',
+  'claustro',
+  'gastoso',
+  'perseguidor',
+  'gengar',
+  'ónix',
+  'drowzee',
+  'hipno',
+  'cangrejo',
+  'rey', 
+  'voltorb'
+] ;
+
+let numeroUsuario = parseInt(prompt("Ingrese un numero"))
+for (let i = 1; i <= numeroUsuario; i++) {
+if (i % 5 === 0); {
+console.log(pokemos[i - 1]);
+    }
+}*/
+/*ejercicio 3*/
+let arreglo = [4, "dos", 8, "tres", 5, 9, 1, "cero"];
+
+for (let i = 0; i < arreglo.length; i++) {
+    if (typeof (parseInt(parseFloat(arreglo[i])) === "numero")); {
+        console.log(arreglo[i]);
+    }
+    
+}
+
+
+
+
